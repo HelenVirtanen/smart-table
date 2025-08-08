b9c07f3516c678d2d2fe5022124147661477e58d
Helen Virtanen <helenvirtanen28@gmail.com>
2025-08-08 11:22:56 +0300

    feat: add event listeners change, reset, and submit

--- src/components/table.js
+++ src/components/table.js
@@ -26,12 +26,23 @@ export function initTable(settings, onAction) {
   });
 
   // @todo: #1.3 —  обработать события и вызвать onAction()
+  root.container.addEventListener('change', () => {
+    onAction();
+  });
+
+  root.container.addEventListener('reset', () => {
+    setTImeout(onAction, 500);
+  });
+
+  root.container.addEventListener('submit', (e) => {
+    e.preventDefault();
+    onAction(e.submitter);
+  });
 
   const render = (data) => {
     // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
     const nextRows = data.map((item) => {
       const row = cloneTemplate(rowTemplate);
-      console.log(row);
 
       Object.keys(item).forEach((key) => {
         if (key in row.elements) {
