fd9c5d36a017c6d99df6498477d6cbe432e4f69d
Helen Virtanen <helenvirtanen28@gmail.com>
2025-08-09 15:57:45 +0300

    feat: add init searching function

--- src/components/searching.js
+++ src/components/searching.js
@@ -3,9 +3,16 @@ import {rules, createComparison} from "../lib/compare.js";
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
+    const compare = createComparison(
+        ["skipEmptyTargetValues"],
+        [rules.searchMultipleFields(searchField, ["date", "customer", "seller"], false)]
+    );
 
-    return (data, state, action) => {
+    return (data, state) => {
         // @todo: #5.2 — применить компаратор
+        if (state.search) {
+            return data.filter(item => compare(item, state));
+        }
         return data;
     }
 }
