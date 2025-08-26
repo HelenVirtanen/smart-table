5248ab4a9b2e9c075320068b032f74e5a5c1e91d
Helen Virtanen <helenvirtanen28@gmail.com>
2025-08-27 02:40:33 +0300

    refactor: add api to main.js, replace result with query in render

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ import { initSorting } from "./components/sorting.js";
 import { initPagination } from "./components/pagination.js";
 
 // Исходные данные используемые в render()
-const { data, ...indexes } = initData(sourceData);
+const api = initData(sourceData);
 
 /**
  * Сбор и обработка полей из таблицы
@@ -36,16 +36,18 @@ function collectState() {
  * Перерисовка состояния таблицы при любых изменениях
  * @param {HTMLButtonElement?} action
  */
-function render(action) {
+async function render(action) {
   let state = collectState(); // состояние полей из таблицы
-  let result = [...data]; // копируем для последующего изменения
+  let query = {};
   // @todo: использование
-  result = applySearching(result, state, action);
-  result = applyFiltering(result, state, action);
-  result = applySorting(result, state, action);
-  result = applyPagination(result, state, action);
+  query = applySearching(query, state, action);
+  query = applyFiltering(query, state, action);
+  query = applySorting(query, state, action);
+  query = applyPagination(query, state, action);
 
-  sampleTable.render(result);
+  const { total, items } = await api.getRecords(query);
+
+  sampleTable.render(items);
 }
 
 const sampleTable = initTable(
@@ -85,4 +87,8 @@ const applyPagination = initPagination(
 const appRoot = document.querySelector("#app");
 appRoot.appendChild(sampleTable.container);
 
-render();
+async function init() {
+  const indexes = await api.getIndexes();
+}
+
+init().then(render);
